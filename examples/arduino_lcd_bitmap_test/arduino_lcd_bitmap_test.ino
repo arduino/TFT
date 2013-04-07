c0b0a826a8e64e3e0c9da40e2a9fc8ae373c8709
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-07 20:16:25 +0200

    test code better suited for Esplora

--- examples/arduino_lcd_bitmap_test/arduino_lcd_bitmap_test.ino
+++ examples/arduino_lcd_bitmap_test/arduino_lcd_bitmap_test.ino
@@ -3,15 +3,24 @@
 #include <SPI.h>
 #include <SD.h>
 
+#include <Esplora.h>
+
 PImage img;
 
 Arduino_GLCD tft = Arduino_GLCD(7, 0, 1); // Esplora pins
 
 void setup() {
-  img = tft.loadImage("parrot.jpg");
-  tft.image(img, 0, 0);
 }
 
 void loop() {
+  Esplora.writeRGB(0, 0, 0);
+  img = tft.loadImage("parrot.bmp");
+  if (img.isValid()) {
+    Esplora.writeGreen(255);
+  }
+  else
+    Esplora.writeRed(255);
+  tft.image(img, 0, 0);
   
+  delay(3000);
 }
