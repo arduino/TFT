b51b514d4aa2056c407c19ae6cb128b30045e8bc
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-07 20:21:32 +0200

    load works! (forgot to initialize SD)

--- examples/arduino_lcd_bitmap_test/arduino_lcd_bitmap_test.ino
+++ examples/arduino_lcd_bitmap_test/arduino_lcd_bitmap_test.ino
@@ -5,11 +5,20 @@
 
 #include <Esplora.h>
 
+#define SD_CS    8  // Chip select line for SD card
+
 PImage img;
 
 Arduino_GLCD tft = Arduino_GLCD(7, 0, 1); // Esplora pins
 
 void setup() {
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("failed!");
+    return;
+  }
+  Serial.println("OK!");
 }
 
 void loop() {
