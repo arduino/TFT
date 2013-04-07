1bef008cf0e437e2d56aed9dfb7f7d5e812ce2f9
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-07 21:09:00 +0200

    uhm, looks like Adafruit_GFX will always need SD library now...

--- examples/arduino_lcd_test/arduino_lcd_test.ino
+++ examples/arduino_lcd_test/arduino_lcd_test.ino
@@ -25,6 +25,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Arduino_GLCD.h> // Hardware-specific library
 #include <SPI.h>
+#include <SD.h>
 
 // Option 1: use any pins but a little slower
 //Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rst);
