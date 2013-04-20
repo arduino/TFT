f04774a64988e3acdf58800f4cb03f4120482c1a
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-20 11:19:55 +0200

    made a version of EsploraGLCDBitmapLogo for Arduino Uno & co.

---
+++ examples/Arduino/GLCDBitmapLogo/GLCDBitmapLogo.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Arduino_GLCD.h> // Hardware-specific library
+
+// pin definition for the Uno
+#define sd_cs  11
+#define lcd_cs 10
+#define dc     9
+#define rst    8  
+
+// pin definition for the Leonardo
+//#define sd_cs  8
+//#define lcd_cs 7
+//#define dc     0
+//#define rst    1  
+
+Arduino_GLCD LCDscreen = Arduino_GLCD(lcd_cs, dc, rst);
+
+PImage logo;
+
+/*
+ * To run this sketch:
+ * - open the sketch folder (Ctrl-K)
+ * - copy the "arduino.bmp" file to an SD
+ * - put the SD into the SD slot of the Arduino LCD module.
+ */
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(sd_cs)) {
+    Serial.println("failed!");
+    return;
+  }
+  Serial.println("OK!");
+  LCDscreen.begin();
+  LCDscreen.background(0);
+
+  logo = LCDscreen.loadImage("arduino.bmp");
+  if (!logo.isValid()) {
+    Serial.println("error while loading arduino.bmp");
+  }
+}
+
+void loop() {
+  if (logo.isValid() == false) {
+    return;
+  }
+  
+  Serial.println("drawing image");
+  int x = random(LCDscreen.width() - logo.width());
+  int y = random(LCDscreen.height() - logo.height());
+  LCDscreen.image(logo, x, y);
+  delay(1500);
+}
