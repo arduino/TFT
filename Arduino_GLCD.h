07a5c47e8f9a96546b37d7b740183b1e22060433
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-23 19:54:03 +0200

    added comments and credits

--- Arduino_GLCD.h
+++ Arduino_GLCD.h
@@ -6,8 +6,12 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 
-
-
+/// The Arduino LCD is a ST7735-based device.
+/// By default, it is mounted horizontally.
+/// Arduino_GLCD class follows the convention of other
+/// Arduino library classes by adding a begin() method
+/// to be called in the setup() routine.
+/// @author Enrico Gueli <enrico.gueli@gmail.com>
 class Arduino_GLCD : public Adafruit_ST7735 {
 public:
   Arduino_GLCD(uint8_t CS, uint8_t RS, uint8_t RST);
@@ -15,6 +19,8 @@ public:
   void begin();  
 };
 
+/// Esplora boards have hard-wired connections with
+/// the Arduino LCD if mounted on the onboard connector.
 #if (USB_VID == 0x2341) && (USB_PID == 0x803C) // are we building for Esplora?
 extern Arduino_GLCD EsploraLCD;
 #endif
