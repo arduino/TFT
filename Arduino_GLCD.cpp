bc48e1144996c005b73eeab58cca85fb20df9c96
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-24 18:25:23 +0200

    added setting of default height/width so they can be read before begin()

--- Arduino_GLCD.cpp
+++ Arduino_GLCD.cpp
@@ -7,6 +7,10 @@ Arduino_GLCD EsploraLCD(7, 0, 1);
 Arduino_GLCD::Arduino_GLCD(uint8_t CS, uint8_t RS, uint8_t RST) 
   : Adafruit_ST7735(CS, RS, RST)
 {
+  // as we already know the orientation (landscape, therefore rotated),
+  // set default width and height without need to call begin() first.
+  _width = ST7735_TFTHEIGHT;
+  _height = ST7735_TFTWIDTH;
 }
 
 void Arduino_GLCD::begin() {
