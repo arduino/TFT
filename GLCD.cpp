dce4d1b4d2aefb3a66b90e0759568ef09fb37214
Enrico Gueli <enrico.gueli@gmail.com>
2013-04-26 17:59:11 +0200

    other renamings

--- GLCD.cpp
+++ GLCD.cpp
@@ -1,10 +1,10 @@
-#include "Arduino_GLCD.h"
+#include "GLCD.h"
 
 #if (USB_VID == 0x2341) && (USB_PID == 0x803C) // are we building for Esplora?
-Arduino_GLCD EsploraLCD(7, 0, 1);
+GLCD EsploraLCD(7, 0, 1);
 #endif
 
-Arduino_GLCD::Arduino_GLCD(uint8_t CS, uint8_t RS, uint8_t RST) 
+GLCD::GLCD(uint8_t CS, uint8_t RS, uint8_t RST) 
   : Adafruit_ST7735(CS, RS, RST)
 {
   // as we already know the orientation (landscape, therefore rotated),
@@ -13,7 +13,7 @@ Arduino_GLCD::Arduino_GLCD(uint8_t CS, uint8_t RS, uint8_t RST)
   _height = ST7735_TFTWIDTH;
 }
 
-void Arduino_GLCD::begin() {
+void GLCD::begin() {
   initR(INITR_REDTAB);
   setRotation(1);
 }
--- GLCD.h
+++ GLCD.h
@@ -8,13 +8,13 @@
 
 /// The Arduino LCD is a ST7735-based device.
 /// By default, it is mounted horizontally.
-/// Arduino_GLCD class follows the convention of other
+/// GLCD class follows the convention of other
 /// Arduino library classes by adding a begin() method
 /// to be called in the setup() routine.
 /// @author Enrico Gueli <enrico.gueli@gmail.com>
-class Arduino_GLCD : public Adafruit_ST7735 {
+class GLCD : public Adafruit_ST7735 {
 public:
-  Arduino_GLCD(uint8_t CS, uint8_t RS, uint8_t RST);
+  GLCD(uint8_t CS, uint8_t RS, uint8_t RST);
 
   void begin();  
 };
@@ -22,7 +22,7 @@ public:
 /// Esplora boards have hard-wired connections with
 /// the Arduino LCD if mounted on the onboard connector.
 #if (USB_VID == 0x2341) && (USB_PID == 0x803C) // are we building for Esplora?
-extern Arduino_GLCD EsploraLCD;
+extern GLCD EsploraLCD;
 #endif
 
 #endif // _ARDUINO_GLCD_H
